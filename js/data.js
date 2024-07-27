d14d703bde71de53799b270e411277e9257f3b22
PainDota <107191194+PainDota@users.noreply.github.com>
2024-07-27 21:18:08 +0500

    Update data.js
    video

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 //video URL
-const vidURL = "https://www.youtube.com/embed/d-gh7z9e0y8?si=wFaiOZEwF_vCw9ms";
+const vidURL = "https://www.youtube.com/embed/t559xebFL74?si=xa_UggrgFu0skDKQ";
 
 // stat Data
 const statsValues = {
