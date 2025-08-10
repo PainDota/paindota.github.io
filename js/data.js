415743c5397dd402600d5b23c99ed2d15ee6755a
Misbah Noor Awan <malikmisbah005@gmail.com>
2025-08-10 08:02:41 +0500

    Immortal Package: Price Update

--- js/data.js
+++ js/data.js
@@ -88,9 +88,7 @@ const proPackage = {
 };
 
 const immortalPackage = {
-    price: '$2000',
-    discountedPrice : '$1700',
-    priceOff : '$300',
+    price: '$2200',
     features: [
         'Unlimited 1 Hour Sessions',
         '3x VOD Reviews', '1x Analysis Tutorial',
