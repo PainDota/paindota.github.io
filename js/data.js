5728880cd2ada3161c0ddecb4a61625c7f0c59c4
PainDota <hid3ntruth@gmail.com>
2026-01-27 16:33:17 +0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -98,8 +98,7 @@ const proPackage = {
 
 const immortalPackage = {
     price: '$2500',
-    discountedPrice:'$2000',
-    priceOff: '$500',
+    
      
    features: [
         'Unlimited 1 Hour Sessions', 
