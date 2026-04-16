668070ac1b6f82921a444f0dee14bd45ceea179d
333811 <mnawan.bese20seecs@seecs.edu.pk>
2026-04-16 14:56:50 +0500

    success stories + fully functional full gallery

---
+++ js/success-stories.js
@@ -0,0 +1,68 @@
+const gallery = document.getElementById("testimonialsGallery");
+const fullGallery = document.getElementById("fullGallery");
+
+// show only first row (6 items)
+const previewLimit = 6;
+
+/* =======================
+   PREVIEW GALLERY
+======================= */
+successStories.slice(0, previewLimit).forEach((img) => {
+
+    const col = document.createElement("div");
+    col.className = "col-6 col-md-2";
+
+    col.innerHTML = `
+        <div class="preview-wrapper position-relative">
+
+            <img 
+                src="images/reviews/ss/${img}"
+                class="preview-img-2"
+                onclick="openImage('images/reviews/ss/${img}')"
+            >
+
+            <img 
+                src="images/icons/uil_focus.svg"
+                class="zoom-overlay"
+            >
+
+        </div>
+    `;
+
+    gallery.appendChild(col);
+});
+
+
+/* =======================
+   FULL MODAL GALLERY
+======================= */
+successStories.forEach((img) => {
+
+    const col = document.createElement("div");
+    col.className = "col-6 col-md-2";
+
+    col.innerHTML = `
+        <div class="preview-wrapper position-relative">
+
+            <img 
+                src="images/reviews/ss/${img}"
+                class="preview-img-2"
+                onclick="openImage('images/reviews/ss/${img}')"
+            >
+
+            <img 
+                src="images/icons/uil_focus.svg"
+                class="zoom-overlay"
+            >
+
+        </div>
+    `;
+
+    fullGallery.appendChild(col);
+});
+
+function openGalleryModal() {
+    bootstrap.Modal.getOrCreateInstance(
+        document.getElementById("galleryModal")
+    ).show();
+}
